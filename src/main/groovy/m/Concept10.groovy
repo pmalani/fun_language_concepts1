253b3a36fe5141d478abc36561100ee0a98e656b
praka <p@m.org>
2025-01-11 19:09:50 -0800

    add comment

--- src/main/groovy/m/Concept10.groovy
+++ src/main/groovy/m/Concept10.groovy
@@ -1,22 +1,23 @@
 package m
 
+// duck typing, if it quacks like a duck...
 class Concept10 {
 	static void main(String[] args) {
 		List ducks = [new Duck(), new RubberDuck()]
 		for (duck in ducks) {
-			println duck.say();
+			println duck.say()
 		}
 	}
 }
 
 class Duck {
 	String say() {
-		return "quack";
+		return "quack"
 	}
 }
 
 class RubberDuck {
 	String say() {
-		return "really?";
+		return "really?"
 	}
 }
